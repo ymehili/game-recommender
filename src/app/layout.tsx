8ec7a5d2ea2dea50749c924e6e40e470ca034458
ymehili <youssefmehili@gmail.com>
2025-05-17 19:17:56 +0200

    Add Google Ads script to layout for ad integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8161806670678434" crossorigin="anonymous"></script>
+      </head>
       <body className={`${inter.className} bg-gray-100 dark:bg-gray-900 min-h-screen`}>
         <PreferencesProvider>
           {children}
